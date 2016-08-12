d265b2f40c2a43a9d7cda25543e22947b622bbcc
chenliam <t.goo.dark@gmail.com>
2016-08-12 16:03:20 -0400

    Initial commit

---
+++ app/src/main/java/com/example/chenl/top10downloader2/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.chenl.top10downloader2;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        private String mFileContents;
+
+        @Override
+        protected String doInBackground(String... params) {
+            mFileContents = downloadXMLFile(params[0]);
+            if (mFileContents == null) {
+                Log.d("DownloadData", "Error Downloading");
+            }
+            return mFileContents;
+        }
+
+        private String downloadXMLFile(String urlPath) {
+            StringBuilder tempBuffer = new StringBuilder();
+            try {
+                URL url = new URL(urlPath);
+            } catch (IOException e) {
+                Log.d("DownloadData", "IO exception reading data: " + e.getMessage());
+            }
+            return null;
+        }
+    }
+}
